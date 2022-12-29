56cce47d548e63e97065fa657c83101e53065ecf
sichoi42 <42.4.sichoi@gmail.com>
2022-12-29 23:39:44 +0900

    :lipstick: prettier stack
    prettier stack

--- containers/stack.hpp
+++ containers/stack.hpp
@@ -5,88 +5,93 @@
 #include <vector>
 
 namespace ft {
-  // FIXME: 추후 ft::vector로 수정 필요
-  template <class T, class Container = std::vector<T> >
-  class stack {
-    public:
-      typedef Container container_type;
-      typedef typename container_type::value_type value_type;
-      typedef typename container_type::size_type size_type;
-      typedef typename container_type::reference reference;
-      typedef typename container_type::const_reference const_reference;
+	// FIXME: 추후 ft::vector로 수정 필요
+	template <class T, class Container = std::vector<T> >
+	class stack {
+	public:
+		typedef Container container_type;
+		typedef typename container_type::value_type value_type;
+		typedef typename container_type::size_type size_type;
+		typedef typename container_type::reference reference;
+		typedef typename container_type::const_reference const_reference;
 
-    protected:
-      container_type c;
+	protected:
+		container_type c;
 
-    public:
-      explicit stack(const container_type& c_type = container_type()) : c(c_type) {}
-      stack(const stack& st) : c(st.c) {}
-      ~stack() {}
+	public:
+		explicit stack(const container_type& c_type = container_type()) : c(c_type) {}
+		stack(const stack& st) : c(st.c) {}
+		~stack() {}
 
-      stack& operator=(const stack& st) {
-        if (this != &st) {
-          c = st.c;
-        }
-        return *this;
-      }
+		stack& operator=(const stack& st) {
+		if (this != &st) {
+			c = st.c;
+		}
+		return *this;
+		}
 
-      bool empty() const {
-        return c.empty();
-      }
-      size_type size() const {
-        return c.size();
-      }
-      reference top() {
-        return c.back();
-      }
-      const_reference top() const {
-        return c.back();
-      }
-      void push(const value_type& val) {
-        c.push_back(val);
-      }
-      void pop() {
-        c.pop_back();
-      }
-  };
+		bool empty() const {
+		return c.empty();
+		}
 
-  template <class T, class Container>
-  bool operator==(const stack<T, Container>& x, const stack<T, Container>& y) {
-    return x.c == y.c;
-  }
+		size_type size() const {
+		return c.size();
+		}
 
-  template <class T, class Container>
-  bool operator!=(const stack<T, Container>& x, const stack<T, Container>& y) {
-    return !(x.c == y.c);
-  }
+		reference top() {
+		return c.back();
+		}
 
-  template <class T, class Container>
-  bool operator<(const stack<T, Container>& x, const stack<T, Container>& y) {
-    return x.c < y.c;
-  }
+		const_reference top() const {
+		return c.back();
+		}
 
-  template <class T, class Container>
-  bool operator<=(const stack<T, Container>& x, const stack<T, Container>& y) {
-    return x.c <= y.c;
-  }
+		void push(const value_type& val) {
+		c.push_back(val);
+		}
 
-  template <class T, class Container>
-  bool operator>(const stack<T, Container>& x, const stack<T, Container>& y) {
-    return x.c > y.c;
-  }
+		void pop() {
+		c.pop_back();
+		}
+	};
 
-  template <class T, class Container>
-  bool operator>=(const stack<T, Container>& x, const stack<T, Container>& y) {
-    return x.c >= y.c;
-  }
+	template <class T, class Container>
+	bool operator==(const stack<T, Container>& x, const stack<T, Container>& y) {
+	return x.c == y.c;
+	}
+
+	template <class T, class Container>
+	bool operator!=(const stack<T, Container>& x, const stack<T, Container>& y) {
+	return !(x.c == y.c);
+	}
+
+	template <class T, class Container>
+	bool operator<(const stack<T, Container>& x, const stack<T, Container>& y) {
+	return x.c < y.c;
+	}
+
+	template <class T, class Container>
+	bool operator<=(const stack<T, Container>& x, const stack<T, Container>& y) {
+	return x.c <= y.c;
+	}
+
+	template <class T, class Container>
+	bool operator>(const stack<T, Container>& x, const stack<T, Container>& y) {
+	return x.c > y.c;
+	}
+
+	template <class T, class Container>
+	bool operator>=(const stack<T, Container>& x, const stack<T, Container>& y) {
+	return x.c >= y.c;
+	}
 }
 
 /*
-  STL Reference
-  https://en.cppreference.com/w/cpp/container/stack
+	STL Reference
+	https://en.cppreference.com/w/cpp/container/stack
 
-  STL Base Header
-  https://en.cppreference.com/w/cpp/header/stack
+	STL Base Header
+	https://en.cppreference.com/w/cpp/header/stack
 */
 
 #endif
