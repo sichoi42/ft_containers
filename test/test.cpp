8bec6c426e1e18c39ee8f391d11211bd10ab3eae
sichoi42 <42.4.sichoi@gmail.com>
2022-12-29 19:03:24 +0900

    :tada: begin ft_containers
    begin ft_containers

---
+++ test/test.cpp
@@ -0,0 +1,19 @@
+#include <stack>
+#include <iostream>
+
+int main()
+{
+    std::stack<int> mystack;
+
+    for (int i=0; i<5; ++i) mystack.push(i);
+
+    std::cout << "Popping out elements...";
+    while (!mystack.empty())
+    {
+        std::cout << ' ' << mystack.top();
+        mystack.pop();
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
