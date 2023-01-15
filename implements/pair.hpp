b8869297beced74dc2b2a870b9d531b403bfdb81
sichoi42 <42.4.sichoi@gmail.com>
2023-01-15 22:46:21 +0900

    :sparkles: add pair
    add pair

---
+++ implements/pair.hpp
@@ -0,0 +1,61 @@
+namespace ft {
+	template <typename T1, typename T2>
+	struct pair {
+		typedef T1 first_type;
+		typedef T2 second_type;
+
+		first_type first;
+		second_type second;
+
+		pair() : first(), second() {}
+		pair(const first_type& a, const second_type& b) : first(a), second(b) {}
+
+		template <typename U1, typename U2>
+		pair(const pair<U1, U2>& p) : first(p.first), second(p.second) {}
+
+		pair& operator=(const pair& p) {
+			if (this == &p) {
+				first = p.first;
+				second = p.second;
+			}
+			return *this;
+		}
+		~pair() {}
+	};
+
+	// Non-member functions
+	template <typename T1, typename T2>
+	ft::pair<T1, T2> make_pair(T1 x, T2 y) {
+		return ft::pair<T1, T2>(x, y);
+	}
+
+	template <typename T1, typename T2>
+	bool operator==(const ft::pair<T1, T2>& x, const ft::pair<T1, T2>& y) {
+		return x.first == y.first && x.second == y.second;
+	}
+
+	template <typename T1, typename T2>
+	bool operator!=(const ft::pair<T1, T2>& x, const ft::pair<T1, T2>& y) {
+		return !(x == y);
+	}
+
+	template <typename T1, typename T2>
+	bool operator<(const ft::pair<T1, T2>& x, const ft::pair<T1, T2>& y) {
+		return x.first < y.first || (!(y.first < x.first) && x.second < y.second);
+	}
+
+	template <typename T1, typename T2>
+	bool operator<=(const ft::pair<T1, T2>& x, const ft::pair<T1, T2>& y) {
+		return !(y < x);
+	}
+
+	template <typename T1, typename T2>
+	bool operator>(const ft::pair<T1, T2>& x, const ft::pair<T1, T2>& y) {
+		return y < x;
+	}
+
+	template <typename T1, typename T2>
+	bool operator>=(const ft::pair<T1, T2>& x, const ft::pair<T1, T2>& y) {
+		return !(x < y);
+	}
+}
