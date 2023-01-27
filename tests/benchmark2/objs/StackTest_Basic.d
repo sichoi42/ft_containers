4c3c1571cf9d8252ea68888ef1f5f94ef56d1a27
sichoi42 <42.4.sichoi@gmail.com>
2023-01-27 20:15:14 +0900

    :sparkles: add test codes
    add test codes

---
+++ tests/benchmark2/objs/StackTest_Basic.d
@@ -0,0 +1,45 @@
+objs/StackTest_Basic.o: srcs/stack/StackTest_Basic.cpp \
+  srcs/stack/../includes/Log.hpp srcs/stack/../includes/StackTest.hpp \
+  srcs/stack/../includes/./UnitTester.hpp \
+  srcs/stack/../includes/./Log.hpp \
+  srcs/stack/../includes/../../../../containers/stack.hpp \
+  ../../containers/./vector.hpp \
+  ../../containers/../implements/enable_if.hpp \
+  ../../containers/../implements/equal.hpp \
+  ../../containers/../implements/is_integral.hpp \
+  ../../containers/../implements/../utils/integral_constant.hpp \
+  ../../containers/../implements/lexicographical_compare.hpp \
+  ../../containers/../implements/random_access_iterator.hpp \
+  ../../containers/../implements/./iterator_traits.hpp \
+  ../../containers/../implements/reverse_iterator.hpp \
+  srcs/stack/../includes/UnitTester.hpp
+
+srcs/stack/../includes/Log.hpp:
+
+srcs/stack/../includes/StackTest.hpp:
+
+srcs/stack/../includes/./UnitTester.hpp:
+
+srcs/stack/../includes/./Log.hpp:
+
+srcs/stack/../includes/../../../../containers/stack.hpp:
+
+../../containers/./vector.hpp:
+
+../../containers/../implements/enable_if.hpp:
+
+../../containers/../implements/equal.hpp:
+
+../../containers/../implements/is_integral.hpp:
+
+../../containers/../implements/../utils/integral_constant.hpp:
+
+../../containers/../implements/lexicographical_compare.hpp:
+
+../../containers/../implements/random_access_iterator.hpp:
+
+../../containers/../implements/./iterator_traits.hpp:
+
+../../containers/../implements/reverse_iterator.hpp:
+
+srcs/stack/../includes/UnitTester.hpp:
